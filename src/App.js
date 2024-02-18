a254afd45608a8f2db30b1448645b2dc2f7900c5
Ritesh <20je0793@am.iitsm.ac.in>
2024-02-18 18:46:03 +0530

    initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React,{useEffect} from "react";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import './App.css';
+
+import Navbar from "./components/navbar";
+import Footer from "./components/Footer";
+import Register from './components/register';
+import Home from "./components/Home";
+import PostQuest from "./components/PostQuest";
+import Applications from "./components/Applicatons";
+import CMRegister from "./components/CMregistrationform";
+import Login from "./components/Login";
+import Myapplication from "./components/Myapplications";
+import Questpage from "./components/Questpage";
+import CMDashboard from "./components/Community_manager_dashbord";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <Navbar/> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cm' element={<CMDashboard/>} />
+        {/* <Route path='/register' element={<Register/>} /> */}
+        <Route path='/register' element={<CMRegister/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/postquest' element={<PostQuest/>}  />
+        <Route path='/applications' element={<Applications/>}  />
+        <Route path='/myapplications' element={<Myapplication/>}  />
+        <Route path='/footer' element={<Footer/>}/>
+        <Route path='/questpage' element={<Questpage/>}/>
+      </Routes>
+    </BrowserRouter>
+    {/* <Footer/> */}
+    </>
   );
 }
 
